4be92b8c20c3620c83baf35b80d6226a811c7cfd
yusuf <yusufrosman@gmail.com>
2018-03-29 08:51:56 +0800

    got my todo done

---
+++ lib/common/dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyDialog extends Dialog {
+
+  const MyDialog({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Container(
+          decoration: new BoxDecoration(
+            border: new Border.all(width: 3.0, color: Colors.white),
+            borderRadius: const BorderRadius.all(const Radius.circular(4.0)),
+            color: Colors.white
+          ),
+            margin: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.0),
+            child: new ConstrainedBox(
+                constraints: const BoxConstraints(minWidth: 280.0),
+                child: new Material(
+                  color: Colors.white,
+                    child:new IntrinsicWidth(
+                          child: new Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              new Center(
+                                  child: child
+                              )
+                            ],
+                          ),
+                        )
+                    )
+            )
+        )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/settings/app_config.dart';
+import 'package:untitled/common/dialog.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'My Todo App'),
     );
   }
 }
@@ -24,40 +24,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var addListController = new TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  List<String> textLists = [];
+  List<bool> textCheckBoxes = [];
+
+  _onClickShowDialog() {
+    Dialog dialog = new MyDialog(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text('Add List'),
+              new TextField(
+                controller: addListController,
+                decoration: new InputDecoration.collapsed(
+                    hintText: 'Add what you want here fam'),
+                maxLines: 3,
+                onSubmitted: (String text) {
+                  Navigator.pop(context, {'value': text});
+                },
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new FlatButton(
+                      onPressed: () {
+                        Navigator.pop(
+                            context, {'value': addListController.text});
+                      }, child: new Text('add')),
+                  new FlatButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }, child: new Text('delete')),
+                ],
+              )
+            ],
+          ),
+        )
+    );
+
+    showDialog(context: context, child: dialog)
+        .then((onValue) {
+      if (onValue == null) {
+        return;
+      }
+
+      textLists.add(onValue['value']);
+      textCheckBoxes.add(false);
+      addListController.clear();
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var config = AppConfig.of(context);
+    deleteRows() {
+      for (var i = 0; i < textLists.length; i++) {
+        if (textCheckBoxes[i]) {
+          textCheckBoxes.removeAt(i);
+          textLists.removeAt(i);
+        }
+      }
+      setState(() {});
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.add),
+              onPressed: _onClickShowDialog),
+          new IconButton(
+              icon: new Icon(Icons.remove),
+              onPressed: deleteRows),
+        ],
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: new Column(
           children: <Widget>[
-            new Text(
-              config.appName,
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Flexible(
+                child: new ListView.builder(
+                    itemCount: textLists.length,
+                    itemBuilder: (context, index) {
+                      return new Row(
+                        children: <Widget>[
+                          new Checkbox(
+                              value: textCheckBoxes[index],
+                              onChanged: (bool newValue) {
+                                textCheckBoxes[index] = newValue;
+                                setState(() {});
+                              }),
+                          new Text(textLists[index]),
+                        ],
+                      );
+                    })
             ),
           ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
     );
   }
 }
